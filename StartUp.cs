9fce9434d2f829b28073e2c48b91786bee81d8a7
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-04-30 18:44:10 +0300

    add Person.cs and StartUp.cs

---
+++ StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+       public static void Main(string[] args)
+        {
+            Person personOne = new Person();
+            personOne.Name = "Peer";
+            personOne.Age = 20;
+
+            Person personTwo = new Person() 
+            {
+                Name = "Kris",
+                Age = 22 
+            };
+
+        }
+    }
+}
